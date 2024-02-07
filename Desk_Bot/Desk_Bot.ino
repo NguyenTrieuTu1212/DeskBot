1778c8919bac114bc65d36977da9491e93561345
NguyenTrieuTu1212 <143092693+NguyenTrieuTu1212@users.noreply.github.com>
2024-02-07 20:47:14 +0700

    Make AP with wemos D1 mini

--- Desk_Bot/Desk_Bot.ino
+++ Desk_Bot/Desk_Bot.ino
@@ -1,17 +1,35 @@
-
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-
+#include <ESP8266WiFi.h>//Import ESP 8266 WiFi library
+#include <WiFiClient.h>//Import WiFi library
+#include <ESP8266WebServer.h> //Import ESP 8266 WiFi Webserver
 
 
 
+#ifndef APSSID
+#define APSSID "microdigisoft"
+#define APPSK  "microdigisoft"
+#endif
 
-void setup(){
+const char *ssid = APSSID;
+const char *password = APPSK;
+ESP8266WebServer server(80);
 
+void handleRoot() {
+  server.send(200, "text/html", "You are connected");
 }
-
-
-void loop(){
-
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+void loop() {
+  server.handleClient();
 }
